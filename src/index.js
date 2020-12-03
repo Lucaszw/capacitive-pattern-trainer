e58d12e4cfac0df711dbb831354e891556edb248
lucaszw <lucas.zeer@gmail.com>
2020-12-04 00:05:18 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@ import './scss/main.scss';
 import _ from 'lodash';
 
 import Navbar from './js/navbar';
-import Home from './js/home';
-import PizzaCreator from "./js/pizza-creator";
 
 const navbar = new Navbar();
-const home = new Home();
-const pizzaCreator = new PizzaCreator();
 
 function main() {
     navbar.draw();
-    pizzaCreator.draw();
-    
-    window.restore = pizzaCreator.restorePizza.bind(pizzaCreator);
 
     navbar.onChangePage = (elem) => {
         const {page} = elem.dataset;
